210eee91170ca24a2c536626520384c02bb4f1d7
bbodo69 <58586282+bbodo69@users.noreply.github.com>
2023-12-12 14:35:31 +0900

    Update Test.py

--- process/Test.py
+++ process/Test.py
@@ -11,44 +11,47 @@ import datetime
 import time
 import matplotlib.pyplot as plt
 
-rootPath = 'C:\Python_Stocks'
-inputFolderPath = os.path.join(rootPath, 'input')
-outputFolderPath = os.path.join(rootPath, 'output')
-resultFolderPath = os.path.join(rootPath, 'result')
-imgFolderPath = os.path.join(rootPath, 'imgMV')
-masterFilePath = os.path.join(inputFolderPath, 'Master.xlsx')
-
-resultFileFolder = "../"
-resultFileName = "dailyConfirmCode.json"
-
-# 코스피 종목 받아오기
-sheetName = 'KOSPI'
-df = excel_collection.readExcelToDataFrame(masterFilePath, sheetName)
-
-# 결과 lst 생성
-lstResult = []
-dfResult = pd.DataFrame(columns=["code"])
-
-for idx, row in df.iterrows() :
-  # 종목 시세 데이터 가져오기
-  if "K" in row['code'] or "L" in row['code'] :
-    continue
-  dfCode = dataProcessing.GetStockPrice(row['code'], 110)
-  dfCode = dataProcessing.standardizationStockSplit(dfCode)
-
-  dfShortMA = dataProcessing.GetMovingAverageRetDF(dfCode, 10)
-  dfLongMA = dataProcessing.GetMovingAverageRetDF(dfCode, 100)
+def testMain() :
+  rootPath = 'C:\Python_Stocks'
+  inputFolderPath = os.path.join(rootPath, 'input')
+  outputFolderPath = os.path.join(rootPath, 'output')
+  resultFolderPath = os.path.join(rootPath, 'result')
+  imgFolderPath = os.path.join(rootPath, 'imgMV')
+  masterFilePath = os.path.join(inputFolderPath, 'Master.xlsx')
   
-  isFlag = dataProcessing.isBetweenTwoMV(dfCode, dfLongMA, dfShortMA, 0)
-
-  print("{0} |||| {1}".format(len(df), idx+1))
+  resultFileFolder = "../"
+  resultFileName = "dailyConfirmCode.json"
   
-  if isFlag :
-    imgFolderPath = os.path.join(rootPath, 'img_Confirm')
-    imgFilePath = os.path.join(imgFolderPath, row['code'])
-    list_row = [row['code']]
-    dfResult.loc[len(dfResult)] = list_row
-    Image.SaveDFImage(row['code'], dfCode, imgFilePath)
+  # 코스피 종목 받아오기
+  sheetName = 'KOSPI'
+  df = excel_collection.readExcelToDataFrame(masterFilePath, sheetName)
+  
+  # 결과 lst 생성
+  lstResult = []
+  dfResult = pd.DataFrame(columns=["code"])
+  
+  for idx, row in df.iterrows() :
+    # 종목 시세 데이터 가져오기
+    if "K" in row['code'] or "L" in row['code'] :
+      continue
+    dfCode = dataProcessing.GetStockPrice(row['code'], 110)
+    dfCode = dataProcessing.standardizationStockSplit(dfCode)
+  
+    dfShortMA = dataProcessing.GetMovingAverageRetDF(dfCode, 10)
+    dfLongMA = dataProcessing.GetMovingAverageRetDF(dfCode, 100)
+    
+    isFlag = dataProcessing.isBetweenTwoMV(dfCode, dfLongMA, dfShortMA, 0)
+  
+    print("{0} |||| {1}".format(len(df), idx+1))
+    
+    if isFlag :
+      imgFolderPath = os.path.join(rootPath, 'img_Confirm')
+      imgFilePath = os.path.join(imgFolderPath, row['code'])
+      list_row = [row['code']]
+      dfResult.loc[len(dfResult)] = list_row
+      Image.SaveDFImage(row['code'], dfCode, imgFilePath)
+  
+  dfResult.to_json(path_or_buf=os.path.join(resultFileFolder, resultFileName), orient="records")
+  print("전체 : {0}, 대상 : {1}".format(len(df), len(lstResult)))
 
-dfResult.to_json(path_or_buf=os.path.join(resultFileFolder, resultFileName), orient="records")
-print("전체 : {0}, 대상 : {1}".format(len(df), len(lstResult)))
+testMain()
