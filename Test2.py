67d9b57ca8bf58070f791304a3452ad74c777cf3
yrkim <zzangkim89@naver.com>
2023-12-18 00:21:55 +0900

    backup

--- Test2.py
+++ Test2.py
@@ -1,16 +1,98 @@
 import json
 import random
+import module.dataProcessing as dataProcessing
 
-dicTest = {1 : "1", 2 : "2"}
+p00_05, p05_10, p10_15, p15_20, p20_25, p25_30, p30_35, p35_40, p40_45, p45_50, p50_55, p55_60, p60_65, p65_70, p75_80 = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+print(p00_05)
+print(p00_05)
 
-print(dicTest)
+dicResult = {}
+keys = ["p00_05", "p05_10", "p10_15", "p15_20", "p20_25", "p25_30", "p30_35", "p35_40", "p40_45", "p45_50", "p50_55", "p55_60", "p60_65", "p65_70", "p75_80", "m00_05", "m05_10", "m10_15", "m15_20", "m20_25", "m25_30", "m30_35", "m35_40", "m40_45", "m45_50", "m50_55", "m55_60", "m60_65", "m65_70", "m75_80"]
+for i in keys :
+    dicResult[i] = 0
+print(keys)
+lists = dicResult.items()
+print(lists)
 
-if 2 in dicTest :
-    print(1)
+#x, y = zip(*keys)
 
-with open("dailyConfirmCode.json", 'rt', encoding='UTF8') as json_file:
-    codes = json.load(json_file)
+try:
+    dfCode = dataProcessing.GetStockPrice("005930", 300)
+    tmpPrice = 0
+    for idx_c, code in dfCode.iterrows():
+        if tmpPrice == 0 :
+            tmpPrice = code['종가']
+            continue
+        else :
+            if 75 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 80:
+                dicResult['p75_80'] += 1
+            elif 70 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 75:
+                dicResult['p70_75'] += 1
+            elif 65 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 70:
+                dicResult['p65_70'] += 1
+            elif 60 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 65:
+                dicResult['p60_65'] += 1
+            elif 55 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 60:
+                dicResult['p55_60'] += 1
+            elif 50 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 55:
+                dicResult['p50_55'] += 1
+            elif 45 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 50:
+                dicResult['p45_50'] += 1
+            elif 40 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 45:
+                dicResult['p40_45'] += 1
+            elif 35 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 40:
+                dicResult['p35_40'] += 1
+            elif 30 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 35:
+                dicResult['p30_35'] += 1
+            elif 25 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 30:
+                dicResult['p25_30'] += 1
+            elif 20 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 25:
+                dicResult['p20_25'] += 1
+            elif 15 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 20:
+                dicResult['p15_20'] += 1
+            elif 10 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 15:
+                dicResult['p10_15'] += 1
+            elif 5 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 10:
+                dicResult['p05_10'] += 1
+            elif 0 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 5:
+                dicResult['p00_05'] += 1
+            elif -5 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 0:
+                dicResult['m00_05'] += 1
+            elif -10 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -5:
+                dicResult['m05_10'] += 1
+            elif -15 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -10:
+                dicResult['m10_15'] += 1
+            elif -20 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -15:
+                dicResult['m15_20'] += 1
+            elif -25 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -20:
+                dicResult['m20_25'] += 1
+            elif -30 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -25:
+                dicResult['m25_30'] += 1
+            elif -35 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -30:
+                dicResult['m30_35'] += 1
+            elif -40 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -35:
+                dicResult['m35_40'] += 1
+            elif -45 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -40:
+                dicResult['m40_45'] += 1
+            elif -50 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -45:
+                dicResult['m45_50'] += 1
+            elif -55 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -50:
+                dicResult['m50_55'] += 1
+            elif -60 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -55:
+                dicResult['m55_60'] += 1
+            elif -65 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -60:
+                dicResult['m60_65'] += 1
+            elif -70 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -65:
+                dicResult['m65_70'] += 1
+            elif -75 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -70:
+                dicResult['m70_75'] += 1
+            elif -80 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -75:
+                dicResult['m75_80'] += 1
 
-print(type(codes))
+        tmpPrice = code['종가']
 
-print(random.sample(codes,50))
+except Exception as e:
+    print(e)
+
+
+
--- process/ValidateStocks.py
+++ process/ValidateStocks.py
@@ -39,6 +39,105 @@ df = excel_collection.readExcelToDataFrame(masterFilePath, sheetName)  # 코스�
 if sampleCnt == 0 :
     sampleCnt = len(df)
 
+def sectionsCount() :
+
+    dicResult = {}
+    keys = ["p75_80", "p70_75", "p65_70", "p60_65", "p55_60", "p50_55", "p45_50", "p40_45", "p35_40", "p30_35",
+            "p25_30", "p20_25", "p15_20", "p10_15", "p05_10", "p00_05", "m00_05", "m05_10", "m10_15", "m15_20", "m20_25",
+            "m25_30", "m30_35", "m35_40", "m40_45", "m45_50", "m50_55", "m55_60", "m60_65", "m65_70", "m70_75", "m75_80"]
+    for i in keys:
+        dicResult[i] = 0
+
+    dfSample = df.sample(sampleCnt).sort_index()
+    dfSample.reset_index()
+
+    tmpPrice = 0
+    for idx, row in dfSample.iterrows():
+        try :
+            dfCode = dataProcessing.GetStockPrice(row['code'], 300)
+            dfCode = dataProcessing.standardizationStockSplit(dfCode)
+            for idx_c, code in dfCode.iterrows():
+                if tmpPrice == 0:
+                    tmpPrice = code['종가']
+                    continue
+                else:
+                    if 75 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 80:
+                        dicResult['p75_80'] += 1
+                    elif 70 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 75:
+                        dicResult['p70_75'] += 1
+                    elif 65 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 70:
+                        dicResult['p65_70'] += 1
+                    elif 60 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 65:
+                        dicResult['p60_65'] += 1
+                    elif 55 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 60:
+                        dicResult['p55_60'] += 1
+                    elif 50 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 55:
+                        dicResult['p50_55'] += 1
+                    elif 45 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 50:
+                        dicResult['p45_50'] += 1
+                    elif 40 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 45:
+                        dicResult['p40_45'] += 1
+                    elif 35 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 40:
+                        dicResult['p35_40'] += 1
+                    elif 30 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 35:
+                        dicResult['p30_35'] += 1
+                    elif 25 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 30:
+                        dicResult['p25_30'] += 1
+                    elif 20 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 25:
+                        dicResult['p20_25'] += 1
+                    elif 15 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 20:
+                        dicResult['p15_20'] += 1
+                    elif 10 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 15:
+                        dicResult['p10_15'] += 1
+                    elif 5 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 10:
+                        dicResult['p05_10'] += 1
+                    elif 0 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 5:
+                        dicResult['p00_05'] += 1
+                    elif -5 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < 0:
+                        dicResult['m00_05'] += 1
+                    elif -10 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -5:
+                        dicResult['m05_10'] += 1
+                    elif -15 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -10:
+                        dicResult['m10_15'] += 1
+                    elif -20 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -15:
+                        dicResult['m15_20'] += 1
+                    elif -25 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -20:
+                        dicResult['m20_25'] += 1
+                    elif -30 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -25:
+                        dicResult['m25_30'] += 1
+                    elif -35 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -30:
+                        dicResult['m30_35'] += 1
+                    elif -40 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -35:
+                        dicResult['m35_40'] += 1
+                    elif -45 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -40:
+                        dicResult['m40_45'] += 1
+                    elif -50 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -45:
+                        dicResult['m45_50'] += 1
+                    elif -55 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -50:
+                        dicResult['m50_55'] += 1
+                    elif -60 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -55:
+                        dicResult['m55_60'] += 1
+                    elif -65 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -60:
+                        dicResult['m60_65'] += 1
+                    elif -70 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -65:
+                        dicResult['m65_70'] += 1
+                    elif -75 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -70:
+                        dicResult['m70_75'] += 1
+                    elif -80 <= (tmpPrice - code['종가']) * 1000 / tmpPrice < -75:
+                        dicResult['m75_80'] += 1
+                tmpPrice = code['종가']
+            print("{0} // {1}".format(len(dfSample), idx))
+        except Exception as e :
+            print(e)
+            print(row['code'])
+
+    print(dicResult)
+    lists = dicResult.items()  # sorted by key, return a list of tuples
+    x, y = zip(*lists)  # unpack a list of pairs into two tuples
+
+    plt.plot(x, y)
+    plt.show()
+
 def saveSortingCode(saveFilePath) :
     '''
     KOSPI 종목을 받아와 필터링 후 json 형태로 파일 저장
@@ -658,5 +757,8 @@ for i in [20, 30, 40, 50, 60]:
     for j in [10, 20, 30, 60]:
         MVAndMostPrice(i, 30, j)
 '''
-for i in [10, 50, 100] :
-    createGraphLineAndScatter(i)
+# for i in [10, 50, 100] :
+#     createGraphLineAndScatter(i)
+
+sectionsCount()
+
