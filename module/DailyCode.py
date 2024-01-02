dcd7e8cb1f971e2d84c62fe36e01377a755b3281
yrkim <zzangkim89@naver.com>
2024-01-02 16:27:40 +0900

    backup

--- module/DailyCode.py
+++ module/DailyCode.py
@@ -14,13 +14,7 @@ import matplotlib.pyplot as plt
 def DailyCode(filePath) :
   rootPath = 'C:\Python_Stocks'
   inputFolderPath = os.path.join(rootPath, 'input')
-  outputFolderPath = os.path.join(rootPath, 'output')
-  resultFolderPath = os.path.join(rootPath, 'result')
-  imgFolderPath = os.path.join(rootPath, 'imgMV')
   masterFilePath = os.path.join(inputFolderPath, 'Master.xlsx')
-  
-  resultFileFolder = "../"
-  resultFileName = "dailyConfirmCode.json"
   
   # 코스피 종목 받아오기
   sheetName = 'KOSPI'
@@ -34,22 +28,13 @@ def DailyCode(filePath) :
     # 종목 시세 데이터 가져오기
     if "K" in row['code'] or "L" in row['code'] :
       continue
-    dfCode = dataProcessing.GetStockPrice(row['code'], 110)
-    dfCode = dataProcessing.standardizationStockSplit(dfCode)
-  
-    dfShortMA = dataProcessing.GetMovingAverageRetDF(dfCode, 10)
-    dfLongMA = dataProcessing.GetMovingAverageRetDF(dfCode, 100)
-    
-    isFlag = dataProcessing.isBetweenTwoMV(dfCode, dfLongMA, dfShortMA, 0)
-  
-    print("{0} |||| {1}".format(len(df), idx+1))
-    
-    if isFlag :
-      imgFolderPath = os.path.join(rootPath, 'img_Confirm')
-      imgFilePath = os.path.join(imgFolderPath, row['code'])
+
+    # ____________조건 넣기
+    if dataProcessing.isDisparity(row['code'], 20, 0.97):
       list_row = [row['code']]
       dfResult.loc[len(dfResult)] = list_row
-      Image.SaveDFImage(row['code'], dfCode, imgFilePath)
-  
+
   dfResult.to_json(path_or_buf=filePath, orient="records")
   print("전체 : {0}, 대상 : {1}".format(len(df), len(lstResult)))
+
+DailyCode("dailyConfirmCode.json")
